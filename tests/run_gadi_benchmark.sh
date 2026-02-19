f82d7c2c1a54d982f578734bd1e0bb93db26ca53
fredjaya <fredjaya1@gmail.com>
2026-02-19 11:32:20 +1100

    get benchmark for platinum genomes n=3

--- tests/run_gadi_benchmark.sh
+++ tests/run_gadi_benchmark.sh
@@ -1,7 +1,13 @@
+#!/bin/bash
+# Run on a persistent session `bash tests/run_gadi_benchmark.sh`
+
 module load nextflow singularity
 nextflow run main.nf \
-    --input dev/RM8398_U1a/giab_small.csv \
+    --input dev/platinum3/plat3_samples.csv \
     --ref /g/data/if89/data_library/Homo_Ref/GRCh38.p14/GCF_000001405.40/GCF_000001405.40_GRCh38.p14_genomic.fna \
     --gadi_account er01 \
     --storage_account "gdata/if89+gdata/er01+scratch/er01" \
+    --download_vep_cache \
+    --vep_species "homo_sapiens" \
+    --vep_assembly "GRCh38" \
     -profile gadi -resume 
